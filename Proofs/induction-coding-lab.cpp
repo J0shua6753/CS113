28ccd33342350fcbb99b8ac9c5d28d3a33f7dc66
Emmanuel Garcia <egarcia@ohlone.edu>
2025-01-29 10:42:17 -0800

    Changes to be committed: 	new file:   .vscode/settings.json 	new file:   Proofs/induction-coding-lab.cpp 	new file:   Proofs/induction-coding-lab.md

---
+++ Proofs/induction-coding-lab.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int SumProg(int n){
+
+}
+
+int prog(n){
+    return 4 + 3*(n-1);
+}
+
+main() {
+    for(int i = 0, i++, i<=5){
+        std::cout << prog(i)
+    }
+
+#    SumProg(43);
+}
